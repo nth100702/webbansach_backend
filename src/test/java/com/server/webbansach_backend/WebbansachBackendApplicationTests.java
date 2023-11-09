2520941043715d988ae5f54939ffa7469fc4c4bb
Trung Hieu Nguyen <nth100702@gmail.com>
2023-11-09 23:20:23 +0700

    tao du an moi

---
+++ src/test/java/com/server/webbansach_backend/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.webbansach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
